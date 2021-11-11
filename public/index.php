9e9a36ee50fb9a9c29aa43478236f47bb9fc52eb
Nir <niritzhak10.devel@gmail.com>
2021-11-11 12:15:56 +0200

    Update the htaccess file

--- public/index.php
+++ public/index.php
@@ -1,4 +1,3 @@
-
 <?php
 
 /**
@@ -11,14 +10,16 @@
  * Routing
  */
 require '../Core/Router.php';
-require '../App/Controllers/Post.php'; 
+
 $router = new Router();
+
 
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+//$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
 $router->add('{controller}/{action}');
-$router->add('{controller}/{id:\d+}/{action}');
+$router->add('admin/{action}/{controller}');
     
 // Display the routing table
 echo '<pre>';
@@ -37,32 +38,5 @@ if ($router->match($url)) {
 } else {
     echo "No route found for URL '$url'";
 }
-
-//Test the Post Controller - Dynamcially 
-
-$class_name = 'Post'; 
-$method_name = 'add' ;
-
-
-
-
-
-if(class_exists($class_name) )
-{
-    $post = new Post()  ;
-
-    if(is_callable([$post, $add]))
-      {
-        $sum =  call_user_func_array([$post, $method_name], [4, 5]); 
-        echo 'Sum = ' . $sum; 
-    } 
-          
-
-
-    
-
-}
-
-
 
 ?>
